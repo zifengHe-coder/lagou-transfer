8029b8e8f11d3258cb1f8f0a56b566ef8f94f914
何梓锋 <zifengHe@126.com>
2022-06-12 10:04:03 +0800

    Initial commit

---
+++ src/main/java/com/lagou/edu/service/impl/TransferServiceImpl.java
@@ -0,0 +1,31 @@
+package com.lagou.edu.service.impl;
+
+import com.lagou.edu.dao.AccountDao;
+import com.lagou.edu.dao.impl.JdbcAccountDaoImpl;
+import com.lagou.edu.factory.BeanFactory;
+import com.lagou.edu.pojo.Account;
+import com.lagou.edu.service.TransferService;
+
+/**
+ * @author 应癫
+ */
+public class TransferServiceImpl implements TransferService {
+
+    private AccountDao accountDao;
+
+    public void setAccountDao(AccountDao accountDao) {
+        this.accountDao = accountDao;
+    }
+
+    @Override
+    public void transfer(String fromCardNo, String toCardNo, int money) throws Exception {
+        Account from = accountDao.queryAccountByCardNo(fromCardNo);
+        Account to = accountDao.queryAccountByCardNo(toCardNo);
+
+        from.setMoney(from.getMoney()-money);
+        to.setMoney(to.getMoney()+money);
+
+        accountDao.updateAccountByCardNo(to);
+        accountDao.updateAccountByCardNo(from);
+    }
+}
---
+++ src/main/java/com/lagou/edu/utils/ConnectionUtils.java
@@ -0,0 +1,29 @@
+package com.lagou.edu.utils;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * 饿汉式实例化
+ */
+public class ConnectionUtils {
+    private ConnectionUtils() {
+    }
+
+    private static ConnectionUtils connectionUtils = new ConnectionUtils();
+
+    public static ConnectionUtils getInstance() {
+        return connectionUtils;
+    }
+
+    private ThreadLocal<Connection> threadLocal = new ThreadLocal<>();//当前线程连接
+
+    public Connection getCurrentThreadConn() throws SQLException {
+        Connection connection = threadLocal.get();
+        if (connection == null) {
+            connection = DruidUtils.getInstance().getConnection();
+            threadLocal.set(connection);
+        }
+        return connection;
+    }
+}
