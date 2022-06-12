5510c7435fb10548f56906061f0da8b6afc09d62
何梓锋 <zifengHe@126.com>
2022-06-12 20:02:08 +0800

    纯注解模式

--- src/main/java/com/lagou/edu/service/impl/TransferServiceImpl.java
+++ src/main/java/com/lagou/edu/service/impl/TransferServiceImpl.java
@@ -1,5 +1,8 @@
 package com.lagou.edu.service.impl;
 
+import com.lagou.edu.anno.MyAutowired;
+import com.lagou.edu.anno.MyService;
+import com.lagou.edu.anno.MyTransactional;
 import com.lagou.edu.dao.AccountDao;
 import com.lagou.edu.pojo.Account;
 import com.lagou.edu.service.TransferService;
@@ -9,10 +12,12 @@ import org.springframework.stereotype.Service;
 /**
  * @author 应癫
  */
-@Service("transferService")
+//@Service("transferService")
+@MyService("transferService")
 public class TransferServiceImpl implements TransferService {
 
-    @Autowired
+//    @Autowired
+    @MyAutowired
     private AccountDao accountDao;
 
     public void setAccountDao(AccountDao accountDao) {
@@ -24,6 +29,7 @@ public class TransferServiceImpl implements TransferService {
     }
 
     @Override
+    @MyTransactional
     public void transfer(String fromCardNo, String toCardNo, int money) throws Exception {
         Account from = accountDao.queryAccountByCardNo(fromCardNo);
         Account to = accountDao.queryAccountByCardNo(toCardNo);
